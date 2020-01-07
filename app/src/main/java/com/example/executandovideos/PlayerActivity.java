2958b95a05efa06cac2eb6da3fdfdf721dbe38ef
Jonathan Silva <silva_jonathan@outlook.com.br>
2020-01-07 19:39:00 -0300

    Projeto VideoView

---
+++ app/src/main/java/com/example/executandovideos/PlayerActivity.java
@@ -0,0 +1,36 @@
+package com.example.executandovideos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    private VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+
+        videoView = findViewById(R.id.videoView);
+
+        //Esconde a statusBar e barra de navegação
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        //Esconde a ActionBar
+        getSupportActionBar().hide();
+
+        //Executar o video com controles padrão do Android
+        videoView.setMediaController(new MediaController(this));
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.video);
+        videoView.start();
+
+    }
+}
